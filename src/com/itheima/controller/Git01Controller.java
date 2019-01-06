f84a5beae81423255a4f46513a22420f8f56dacb
secret <605036049@qq.com>
2019-01-06 15:59:42 +0800

    first_git_project

---
+++ src/com/itheima/controller/Git01Controller.java
@@ -0,0 +1,11 @@
+package com.itheima.controller;
+
+/**
+ * @author qinshiji
+ * @data 2019/1/6 15:58
+ */
+public class Git01Controller {
+    public static void main(String[] args) {
+        System.out.println("good boy!");
+    }
+}
