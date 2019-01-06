fa8c5daa5e14d4e3248fadfda466b07f3b16c0c3
secret <605036049@qq.com>
2019-01-06 16:37:46 +0800

    Merge remote-tracking branch 'remotes/origin/b1_nb'
    # Conflicts:
    #	src/com/itheima/controller/Git02Controller.java

---
+++ src/com/itheima/controller/Git05Controller.java
@@ -0,0 +1,13 @@
+package com.itheima.controller;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author qinshiji
+ * @data 2019/1/6 16:36
+ */
+public class Git05Controller {
+    public static void main(String[] args) {
+        System.out.println("new Name");
+    }
+}
